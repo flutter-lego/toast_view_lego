2c7d40d0e454a2367b94dba1c7211d0260a0a4f1
june lee <melodysdreamj@gmail.com>
2024-05-23 19:54:53 +0900

    Initial commit

---
+++ lib/util/_/build_app/function/before_material_app/toast_view_lego/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForToastViewLego(BuildContext context) async {}
